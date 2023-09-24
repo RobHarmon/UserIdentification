fb4e92421e2487b769e065939cffb5f278be8595
RobHarmon <73800745+RobHarmon@users.noreply.github.com>
2023-09-24 11:45:34 -0700

    Update UIQuery.sql

--- UIQuery.sql
+++ UIQuery.sql
@@ -8,28 +8,23 @@ select 4,	'C', 	'3',	'DD'	union all
 select 5,	'E', 	'4',	'EE'	union all
 select 6,	null,	'5',	'EE'
 	)
+
 	,stack
 AS (
 	--we're pivoting the table, makes life easier
-	SELECT IPHash
-		,nest(rn) AS rnarry
-	FROM src
-	GROUP BY ALL
+     SELECT
+       field ||
+       CASE
+       WHEN field = 'IPHash' THEN IPHash::string
+       WHEN field = 'Cookie' THEN Cookie::string
+       WHEN field = 'UserID' THEN UserID::string
+       END AS value,
+       nest(rn) AS rnarry
+     FROM src UNNEST (['IPHash', 'Cookie', 'UserID'] field)
+     GROUP BY ALL
+   )	
+
 	
-	UNION
-	
-	SELECT Cookie
-		,nest(rn) AS rnarry
-	FROM src
-	GROUP BY ALL
-	
-	UNION
-	
-	SELECT UserID
-		,nest(rn) AS rnarry
-	FROM src
-	GROUP BY ALL
-	)
 	,transition
 AS (
 	-- here we create some arrays, joining the pivot to itself where rows are shared among the individual identifiers
